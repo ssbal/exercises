4e5766cc3a18cfc3a80e0c91f5a4ed59a7f2cc62
Sukhdeep <sukhdeep29723@gmail.com>
2023-08-24 12:22:32 +0530

    Update nested loop
    If not divisible upto half of the length, no need to check further

--- checkPrime.js
+++ checkPrime.js
@@ -2,7 +2,8 @@ const getPrimeUpto = prompt("Enter a value upto which you want prime numbers");
 
 for (let i = 2; i <= getPrimeUpto; i++) {
   let prime = true;
-  for (let j = 2; j < i; j++) {
+  // Divisibility only needs to be checked upto half of the number itself
+  for (let j = 2; j <= parseInt(i / 2); j++) {
     if (i % j === 0) prime = false;
   }
 
